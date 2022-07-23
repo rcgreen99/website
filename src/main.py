113e4febd548f8b34e3ba8c8197730ad1d87b4a3
Reese Green <rcgreen99@gmail.com>
2022-07-23 12:31:42 -0600

    add jinja2 template support

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def homepage():
-    return {"message": "Hello World"}
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/blog")
+async def blog(request: Request):
+    return templates.TemplateResponse("blog.html", {"request": request})
+
+
+@app.get("/work")
+async def work(request: Request):
+    return templates.TemplateResponse("work.html", {"request": request})
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
